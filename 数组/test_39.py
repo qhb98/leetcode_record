c6e5d0d619f50ce60ac5a3b15548b8c6df791408
qhb98 <2651311819@qq.com>
2022-08-10 12:23:55 +0800

    8.10 回溯算法

--- 数组/test_39.py
+++ 数组/test_39.py
@@ -3,6 +3,33 @@
 # @Time: 2022/8/9 21:48
 # @Author: QHB
 
+"""
+
+回溯算法: 进行选择; 递归; 撤回选择
+
+解决  排列问题  子集问题 组合问题  切割问题  棋盘问题(n皇后 解数独)
+
+    void backtracking(parameters){
+         if (终止条件) {
+            收集结果
+            return
+         }
+
+         # 进入单层搜索的逻辑
+         for (集合的元素){
+            处理节点
+            递归函数
+            回溯操作 -- 撤销处理节点的情况
+         }
+         return
+
+    }
+
+参考学习链接:
+https://www.bilibili.com/video/BV1cy4y167mM?spm_id_from=333.337.search-card.all.click&vd_source=7111d4cfa9354342c253c06ecdd64e2f
+
+"""
+
 
 def combination_sum(candidates, target):
     """
@@ -20,26 +47,26 @@ def combination_sum(candidates, target):
         return res
 
     for i in range(len(candidates)):
-        backtrack(i, tmp_sum=candidates[i], tmp=[candidates[i]], res=res, target=target, candidates=candidates)
+        backtrack(cur=candidates[i], candidates=candidates, res=res, target=target, cur_result=[candidates[i]],
+                  cur_sum=candidates[i])
     return res
 
 
-def backtrack(i, tmp_sum, tmp, res, target, candidates):
-    if tmp_sum > target or i >= len(candidates):
-        tmp.pop()
+def backtrack(cur, candidates, res, target, cur_result, cur_sum):
+    if cur_sum == target:
+        res.append(cur_result[:])
         return
 
-    if tmp_sum == target:
-        res.append(tmp)
+    if cur_sum > target or cur_sum + min(candidates) > target:
         return
 
-    for j in range(i, len(candidates)):
-        if tmp_sum + candidates[j] > target:
-            break
-        tmp.append(candidates[j])
-        backtrack(j, tmp_sum=tmp_sum + candidates[j], tmp=tmp, res=res, target=target, candidates=candidates)
-        tmp.pop()
-        tmp_sum = tmp_sum - candidates[j]
+    for j in range(candidates.index(cur), len(candidates)):
+        cur_sum += candidates[j]
+        cur_result.append(candidates[j])
+        backtrack(cur=candidates[j], candidates=candidates, res=res, target=target, cur_result=cur_result,
+                  cur_sum=cur_sum)
+        cur_sum -= candidates[j]
+        cur_result.pop()
 
 
 s_sum = combination_sum(candidates=[2, 3, 6, 7], target=7)
